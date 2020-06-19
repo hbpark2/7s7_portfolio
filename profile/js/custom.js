634a5704de0458f971dc1341df06084b214e9504
hbpark2 <hbpark2@naver.com>
2020-06-19 16:23:23 +0900

    fixfixfix

--- profile/js/custom.js
+++ profile/js/custom.js
@@ -1,25 +1,21 @@
 $(document).ready(function () {
 
+    var el = document.querySelector('.main-text')
+    var options = {
+        text: '안 녕 하 세 요 ! \n 웹 ─ 퍼 블 리 셔 박 형 빈 의 \n 포 트 폴 리 오 페 이 지 입 니 다.',
+        textSpeed: 110,
+        blinkSpeed: 0.06
+    }
 
+    var instance = new tinytyper(el, options);
 
-            var el = document.querySelector('.main-text')
-            var options = {
-                text: '안 녕 하 세 요 ! \n 웹 ─ 퍼 블 리 셔 박 형 빈 의 \n 포 트 폴 리 오 페 이 지 입 니 다.',
-                textSpeed: 110,
-                blinkSpeed: 0.06
-            }
-
-            var instance = new tinytyper(el, options);
-
-
-        //변수 ht에 브라우저의 높이값을 저장
-        var ht = $(window).height();
+    //변수 ht에 브라우저의 높이값을 저장
+    var ht = $(window).height();
     //브라우저의 높이값을 section의 높이값으로 지정
     $("section").height(ht);
 
     //브라우저가 리사이즈 될 때마다 브라우저와 section의 높이값을 갱신
     $(window).on("resize", function () {
-        var ht = $(window).height();
         $("section").height(ht);
     });
 
@@ -28,11 +24,8 @@ $(document).ready(function () {
 
     //메뉴 버튼 클릭시
     $("#menu li").on("click", function (e) {
-        e.preventDefault();
-        //
+        e.preventDefault(); // a태그 기본속성 삭제
 
-        //변수 ht에 브라우저의 높이값 저장
-        var ht = $(window).height();
 
         //변수 i에 현재 클릭한 li의 순서값을 저장
         var i = $(this).index();
@@ -51,11 +44,6 @@ $(document).ready(function () {
 
     $(window).on("scroll", function () {
 
-//        console.log("window.scrollTop() : "+ $(window).scrollTop())
-
-        //변수 ht에 현재 브라우저의 넓이값 저장
-        var ht = $(window).height();
-
         //변수 scroll에 현재 문서가 스크롤된 거리 저장
         var scroll = $(window).scrollTop();
         // scroll이 0보다 크면 메뉴가 안보이는 조건문
@@ -68,14 +56,13 @@ $(document).ready(function () {
                 opacity: 0
             }, 500)
         }
-        if (scroll > 4000 && scroll<5000) {
+        if (scroll > 4000 && scroll < 5000) {
             $("#menu a").css({
-                color:'#fff'
+                color: '#fff'
             })
-        }
-        else{
-             $("#menu a").css({
-                color:'#333'
+        } else {
+            $("#menu a").css({
+                color: '#333'
             })
         }
         for (var i = 0; i < 7; i++) {
@@ -95,25 +82,25 @@ $(document).ready(function () {
         //마우스 휠을 올렸을때	
         if (wheelData_c > 0 || wheelData_f < 0) {
             var $prev = $(this).prev().offset().top;
-//            console.log($prev)
+            //            console.log($prev)
             //문서 전체를 prev에 저장된 위치로 이동
-            if($(window).scrollTop()>960){
+            if ($(window).scrollTop() > 960) {
                 $("html,body").stop().animate({
-                "scrollTop": $prev
-                }, 300);
+                    "scrollTop": $prev
+                }, 500);
             }
             return false;
 
-        //마우스 휠을 내렸을때	 
-        } else if (wheelData_c < 0 && $(window).scrollTop()<5000 || wheelData_f > 0) {
+            //마우스 휠을 내렸을때	 
+        } else if (wheelData_c < 0 && $(window).scrollTop() < 5000 || wheelData_f > 0) {
 
             //변수 next에 현재 휠을 움직인 section에서 다음 section의 offset().top위치 저장
             var $next = $(this).next().offset().top;
             //문서 전체를 next에 저장된 위치로 이동
-            
-                $("html,body").stop().animate({
-                    "scrollTop": $next
-                }, 300);
+
+            $("html,body").stop().animate({
+                "scrollTop": $next
+            }, 500);
             return false;
 
         }
@@ -122,3 +109,4 @@ $(document).ready(function () {
 
 
 });
+
