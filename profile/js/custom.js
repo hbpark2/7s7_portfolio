325f6fc43024c794ac8241781128249121bf6e90
hbpark2 <hbpark2@naver.com>
2020-06-19 15:08:47 +0900

    Update custom.js

--- profile/js/custom.js
+++ profile/js/custom.js
@@ -100,7 +100,7 @@ $(document).ready(function () {
             if($(window).scrollTop()>960){
                 $("html,body").stop().animate({
                 "scrollTop": $prev
-                }, 500);
+                }, 300);
             }
             return false;
 
@@ -113,7 +113,7 @@ $(document).ready(function () {
             
                 $("html,body").stop().animate({
                     "scrollTop": $next
-                }, 500);
+                }, 300);
             return false;
 
         }
